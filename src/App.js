9beffafab38be1d5dac9f303ec358872616660d8
tejaspatil159 <117001605+tejaspatil159@users.noreply.github.com>
2023-12-21 18:00:29 +0530

    usenavigate and Nomatch route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Navbar from "./Components/Navbar";
+import OrderSummary from "./Components/OrderSummary";
+import NoMatch from "./Components/NoMatch";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="order-summary" element={<OrderSummary />} />
+        <Route path="*" element={<NoMatch />} />
       </Routes>
     </>
   );
