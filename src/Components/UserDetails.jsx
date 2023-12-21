82b58b9d4ba995c23d5051baa6554b98fac0c298
tejaspatil159 <117001605+tejaspatil159@users.noreply.github.com>
2023-12-21 23:29:14 +0530

    useParams

--- src/Components/UserDetails.jsx
+++ src/Components/UserDetails.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const UserDetails = () => {
-  return <div>UserDetails</div>;
+  const { userId } = useParams();
+  return <div>Details of user {userId}</div>;
 };
 
 export default UserDetails;
