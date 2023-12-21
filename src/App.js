3fcb5524f6eb84a53ce0e6b5df0e28926d92ec9c
tejaspatil159 <117001605+tejaspatil159@users.noreply.github.com>
2023-12-21 17:13:38 +0530

    Link

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Components/Home";
 import About from "./Components/About";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />{" "}
+        <Route path="about" element={<About />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
